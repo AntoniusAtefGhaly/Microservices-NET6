e079e0146f8d7c819c6f1c05d1b161304772107f
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2023-02-22 17:00:01 +0200

    EventBusRabbitMQProducer

---
+++ MicroservicesAPP/src/Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,23 @@
+﻿using EventBusRabbitMQ.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _connection;
+
+        public EventBusRabbitMQProducer(IRabbitMQConnection connection)
+        {
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        public void PublishBasketCheckout(string queueName, BasketCheckoutEvent publishModel)
+        {
+        }
+    }
+}
